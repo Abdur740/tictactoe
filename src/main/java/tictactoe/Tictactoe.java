3f74faba6f11432af648c8f56dd3ff9543dd42b0
Abdur740 <123439024+Abdur740@users.noreply.github.com>
2023-02-13 18:23:33 +0530

    Initial commit

---
+++ src/main/java/tictactoe/Tictactoe.java
@@ -0,0 +1,175 @@
+package tictactoe;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+class gamesetup{
+    private static final Logger log = Logger.getLogger("InfoLogging");
+    Scanner sc = new Scanner(System.in);
+    char [][] arr = new char[3][3];
+ 
+
+    String dia1="";
+    String dia2="";
+    String row1="";
+    String row2="";
+    String row3="";
+  
+    gamesetup(){
+        char value = 49;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                arr[i][j] = value;
+                value++;
+                log.info(arr[i][j]+" | ");
+            }
+            log.info("\n");
+        }
+    }
+    void playgame(){
+        log.info("Enter 'x' or 'o' in the rows and columns to play.\n");
+        log.info("Select the box number: ");
+        char boxvalue = sc.next().charAt(0);   
+        log.info("Enter 'x' or 'o': ");
+        char player = sc.next().charAt(0);   
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(boxvalue == arr[i][j]) {
+                arr[i][j] = player;       
+            }
+        }
+    }
+}
+void verticalcheck()
+{
+    String col1 = "";
+    String col2 = "";
+    String col3 = "";
+    for(int i=0;i < 3;i++){
+        for(int j=0;j<3;j++){
+            if(i==0){
+                col1 = col1+arr[j][i];
+            }
+            else if(i==1){
+                col2 = col2+arr[j][i];
+            }
+            else{
+                col3 = col3+arr[j][i];
+            }
+        }if(col1.equals("xxx")){
+            log.info("Player x wins!");
+        }else if(col1.equals("ooo")){
+            log.info("Player o wins!");
+        }
+        if(col2.equals("xxx")){
+            log.info("Player x wins!");
+        }else if(col2.equals("ooo")){
+            log.info("Player o wins!");
+        }
+        if(col3.equals("xxx")){
+            log.info("Player x wins!");
+        }else if(col3.equals("ooo")){
+            log.info("Player o wins!");
+        }
+        col1 = "";
+        col2 = "";
+        col3 = "";
+    }
+}
+    void horizontalcheck(){
+        for(int i=0;i < 3;i++){
+            for(int j=0;j<3;j++){
+                if(i==0){
+                    row1 = row1+arr[i][j];
+                }
+                else if(i==1){
+                    row2 = row2+arr[i][j];
+                }
+                else{
+                    row3 = row3+arr[i][j];
+                }
+            }if(row1.equals("xxx")){
+                log.info("Player x wins!");
+            }else if(row1.equals("ooo")){
+                log.info("Player o wins!");
+            }
+            if(row2.equals("xxx")){
+                log.info("Player x wins!");
+            }else if(row2.equals("ooo")){
+                log.info("Player o wins!");
+            }
+            if(row3.equals("xxx")){
+                log.info("Player x wins!");
+            }else if(row3.equals("ooo")){
+                log.info("Player o wins!");
+            }
+            row1 = "";
+            row2 = "";
+            row3 = "";
+        }
+    }
+   
+    void diagonalcheck()
+    {
+        int j=0;
+        dia1="";
+        dia2="";
+    
+        for(int i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+                if(i==j)
+                {
+                    dia1=dia1+arr[i][j];
+                  
+                }
+                if(i+j==2)
+                {
+                    dia2=dia2+arr[i][j];
+                }
+            }
+            if(dia1.equals("xxx"))
+            {
+             log.info("\nx wins!");
+            }
+            else if(dia1.equals("ooo")){
+                log.info("o wins!");
+            }
+            if(dia2.equals("xxx"))
+            {
+             log.info("\nx wins!");
+            }else if(dia2.equals("ooo")){       
+            log.info("\no wins!");
+            }
+        
+            
+            
+        }
+        
+    }
+    void displayRes(){
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                log.info(arr[i][j]+" ");
+            }
+            log.info("\n");
+        }
+    }
+}
+class Tictactoe{
+    private static final Logger log = Logger.getLogger("InfoLogging");
+    public static void main(String[] args) {
+        int playertime = 0;
+        gamesetup gs = new gamesetup();
+        do{
+            gs.playgame();
+            gs.displayRes();
+            gs.verticalcheck();
+            gs.horizontalcheck();
+            gs.diagonalcheck();
+            playertime++;
+        }while(playertime!=9);
+    }
+}
