ecc287aef8fa394168bcd8776f6cc21b7ccc7a73
Abdur740 <123439024+Abdur740@users.noreply.github.com>
2023-03-02 17:14:54 +0530

    Update Tictactoe.java

--- src/main/java/tictactoe/Tictactoe.java
+++ src/main/java/tictactoe/Tictactoe.java
@@ -1,16 +1,8 @@
-package tictactoe;
 
-class Tictactoe{
-    public static void main(String[] args) {
-        int playertime = 0;
-        Gamesetup gs = new Gamesetup();
-        do{
-            gs.playgame();
-            gs.displayRes();
-            gs.verticalcheck();
-            gs.horizontalcheck();
-            gs.diagonalcheck();
-            playertime++;
-        }while(playertime!=9);
+public class Tictactoe{
+    public static void start() {
+        Gamesetup gf=new Gamesetup();
+        gf.gameBoard();
+        gf.fillGameBoard();
     }
 }
